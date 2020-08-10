2ebdabceb01c1f1fb4f200a2be0513e72fa23e76
Iain Robertson <iainrobertzon@gmail.com>
2020-08-10 20:08:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,9 @@ def key_for_min_value(name_hash)
   min_value = 999
   min_key= ""
   name_hash.each do |key, value|
-    if value < min_value
+    if value < min_value 
       min_key = key
+    end 
   end 
   min_key
 end
